fddce1ea06333654eac33083b2873fa0a21917d9
liuchenlu <liucl@hua-cloud.com.cn>
2025-06-30 15:21:47 +0800

    [feat]更改签名

--- front-mini-programe/components/tabbar/tabbar.vue
+++ front-mini-programe/components/tabbar/tabbar.vue
@@ -21,7 +21,7 @@ const tabbarItems = [
   {
     pagePath: '/pages/scan/scan',
     text: '扫码',
-    iconPath: '/static/tab_icons/scan.png',
+    iconPath: '/static/tab_icons/scan-active.png',
     selectedIconPath: '/static/tab_icons/scan-active.png'
   },
   {
@@ -33,27 +33,45 @@ const tabbarItems = [
 ]
 //点击tabbar按钮
 const handleTabbarItemClick = (item, index) => {
+	console.log('lucy== token',useStore.token);
   if (useStore.activeTab !== index) {
     //如果点击的是扫描按钮
+	// pileId=2&code=02&stationName=宝安新安充电站 草料二维码，选中文本输入左侧内容即可，具体参数根据实际场景替换
     if (index === 1) {
-      uni.scanCode({
-        success(res) {
-          const scanUrl = res.result
-          const regExp = /pileId=(\d+)&code=(\d+)&stationName=([^&]+)/;
-          const matchResult = scanUrl.match(regExp)
-          let pileId = ''
-          let code = ''
-          let stationName = ''
-          if (matchResult) {
-            pileId = matchResult[1]
-            code = matchResult[2]
-            stationName = matchResult[3]
-          }
-          uni.reLaunch({
-            url: '/subpkg/charging/charging?pileId=' + pileId + '&code=' + code+'&stationName='+stationName
-          })
-        }
-      })
+		if(useStore.token){
+			uni.scanCode({
+			  success(res) {
+			    const scanUrl = res.result
+			    const regExp = /pileId=(\d+)&code=(\d+)&stationName=([^&]+)/;
+			    const matchResult = scanUrl.match(regExp)
+			    let pileId = ''
+			    let code = ''
+			    let stationName = ''
+			    if (matchResult) {
+			      pileId = matchResult[1]
+			      code = matchResult[2]
+			      stationName = matchResult[3]
+			    }
+			    uni.reLaunch({
+			      url: '/subpkg/charging/charging?pileId=' + pileId + '&code=' + code+'&stationName='+stationName
+			    })
+			  }
+			})
+		}else{
+			uni.showModal({
+				title:'未登录',
+				content: '请先登录后再进行扫码操作',
+				confirmText: '去登录',
+				 success: (res) => {
+					 if (res.confirm) {
+						 useStore.setActive(2)
+						 uni.switchTab({
+						 	url:'/pages/my/my'
+						 })
+					 }
+				 }
+			})
+		}
     } else {
       useStore.setActive(index)
       const path = item.pagePath
